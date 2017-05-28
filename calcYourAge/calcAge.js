ded4d9e3ae5e592e36da97287c62d3e3306dcc2c
Ahmed Salah <GeekAhmeds@Gmail.com>
2017-05-28 02:57:16 +0200

    #1

--- calcYourAge/calcAge.js
+++ calcYourAge/calcAge.js
@@ -1,7 +1,11 @@
 var myInput = document.getElementById('basic-url'),
 myDiv   = document.getElementById('myDiv');
+myDiv1   = document.getElementById('myDiv1');
 
- myInput.onkeyup = function () {
-   "use strict";
-   myDiv.innerHTML = myInput.value * 365 ;
- }
+myInput.onkeyup = function () {
+  "use strict";
+  myDiv.innerHTML = myInput.value * 365 + ' Days' ;
+  myDiv1.innerHTML = myInput.value * 365 * 24 + ' Hours' ;
+  myDiv2.innerHTML = myInput.value * 365 * 24 * 60 + ' Min';
+
+}
