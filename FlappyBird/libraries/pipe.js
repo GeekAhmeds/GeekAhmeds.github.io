13de004bb427084e16c961e98ba7731b54bb5f01
Ahmed Salah <GeekAhmeds@Gmail.com>
2017-05-30 16:10:40 +0200

    #2
    Some Updates And Add Clappy, Sound Effect

--- FlappyBird/libraries/pipe.js
+++ FlappyBird/libraries/pipe.js
@@ -1,8 +1,11 @@
 function Pipe() {
-    this.top = random(height/2);
-    this.bottom = random(height/2);
+
+    var spacing = random(70, height / 2);
+    var centry = random(spacing, height - spacing);
+  //  this.top = centry - spacing / 1 ;
+    this.bottom = height- (centry + spacing / 2) ;
     this.x = width;
-    this.w = 30;
+    this.w = random(120);
     this.speed = 2;
     this.highlight = false;
     this.hits = function(bird){
@@ -16,9 +19,11 @@ function Pipe() {
 }
 
     this.show = function () {
+      noStroke()
         fill(191, 191, 191);
         if(this.highlight){
             fill(255, 0, 0);
+          //  song.play();
         }
         rect(this.x, 0, this.w, this.top);
         rect(this.x, height-this.bottom, this.w, this.bottom);
@@ -32,5 +37,5 @@ function Pipe() {
                }else{
                    return false;
                }
-           }     
+           }
 }
--- FlappyBird/libraries/sketch.js
+++ FlappyBird/libraries/sketch.js
@@ -1,22 +1,34 @@
 var bird;
-this.pipes = [];
+var pipes = [];
+var mic;
+var sliderTop;
+var sliderBottom;
+var clapping = false;
+
 function setup(){
-    createCanvas(800, 600);
+//  song = loadSound('beep.mp3');
+///////// If You Want To Make Sound Beep When Bird Hit Pipes Clear This Between The Quotation " // " From Above The Line
+///// And Go To  " Pipe.js " And Go To Line 26 " song.play() " And Clear This Between The Quotation.
+    createCanvas(400, 600);
+    mic = new p5.AudioIn();
+    mic.start();
     bird = new Bird();
     pipes.push  (new Pipe());
+    sliderTop = createSlider(0, 1, 0.3, 0.01);
+    sliderBottom = createSlider(0, 1, 0.1, 0.01);
+
 }
 function draw() { // Background Color and Other
     background(0);
-    bird.update();
-    bird.show();
 
-    if (frameCount %100 == 0 ) {
-        pipes.push(new Pipe());
-    }
+    var vol = mic.getLevel();
+
+
+
 
 for (var i = pipes.length-1; i >= 0; i--) {
     pipes[i].show();
-    pipes[i].update();    
+    pipes[i].update();
     if(pipes[i].hits(bird)){
         console.log("HIT");
 
@@ -27,6 +39,41 @@ for (var i = pipes.length-1; i >= 0; i--) {
     }
 }
 
+    bird.update();
+    bird.show();
+
+    if (frameCount %100 == 0 ) {
+        pipes.push(new Pipe());
+    }
+
+    var thresholdTop = sliderTop.value();
+    var thresholdBottom = sliderBottom.value();
+
+
+
+if (vol > thresholdTop && !clapping) {
+  bird.up();
+  clapping = true;
+}
+  if (vol < thresholdBottom) {
+    clapping = false;
+  }
+
+    fill(0, 255, 0);
+    noStroke();
+    var y = map(vol, 0, 1, height, 0);
+    rect(width-50, y, 50, height-y);
+
+
+    var ty = map(thresholdTop, 0, 1, height, 0);
+    stroke(255, 0, 0);
+    strokeWeight(4);
+    line(width -50, ty, width, ty);
+
+    var bottomy = map(thresholdBottom, 0, 1, height, 0);
+    stroke(0, 0, 255);
+    strokeWeight(4);
+    line(width -50, bottomy, width, bottomy);
 }
     function keyPressed() {
         if (key ==  ' ' ) {
@@ -34,4 +81,3 @@ for (var i = pipes.length-1; i >= 0; i--) {
         //    console.log("SPACE")
         }
     }
-
